53496b77671eb4f9368f0257c10cc2ebe8eba2e2
Jonathon Belotti <jbelotti@zendesk.com>
2017-04-23 02:29:38 +1000

    add folder structure for improved user analysis functionality

---
+++ maicroft/intelligence/gender/classify.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Goal: Given data about a Twitter or Reddit user, such as tweets, comments,
+      interests, subreddits etc. predict whether that user is:
+      - a man (gender)
+      - a woman (gender)
+      - non-binary (gender)
+      - unknown (low confidence prediction)
+"""
