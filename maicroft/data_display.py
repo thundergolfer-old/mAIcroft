277021d2d84726deb6474c3e3752a24c41bc4298
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2018-03-03 15:21:22 +1100

    small cleanup in data_display.py

--- maicroft/data_display.py
+++ maicroft/data_display.py
@@ -1,9 +1,7 @@
-
-from maicroft.users.twitter_user import TwitterUser
 from maicroft.users.reddit_user import RedditUser
 
 """
-Basic Social Content display functions to aid in data understanding,
+Basic functions to aid in data understanding,
 debugging, and presentation
 """
 
@@ -14,8 +12,3 @@ def display_user_tweets(twitter_user, limit=15):
         print str(i) + ": " + tweet.text.encode('utf-8')
         if i == limit:
             break
-
-
-if __name__ == '__main__':
-    user = TwitterUser('KimKardashian')
-    display_user_tweets(user)
